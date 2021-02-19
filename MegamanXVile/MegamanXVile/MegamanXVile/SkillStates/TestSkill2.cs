91ef1a346a7a8e87a08f10cc8ef507eb20247948
BLKNeko <69521435+BLKNeko@users.noreply.github.com>
2021-02-19 14:44:19 -0300

    test skill2 now shoot 3 derections !
    finalmente ! eu estava usando números muito grandes as direções vão de 0 até 1.0

--- MegamanXVile/MegamanXVile/MegamanXVile/SkillStates/TestSkill2.cs
+++ MegamanXVile/MegamanXVile/MegamanXVile/SkillStates/TestSkill2.cs
@@ -50,17 +50,19 @@ namespace MegamanXVile.SkillStates
                 base.characterBody.AddSpreadBloom(0.15f);
                 Ray aimRay = base.GetAimRay();
                 EffectManager.SimpleMuzzleFlash(EntityStates.Commando.CommandoWeapon.FirePistol.effectPrefab, base.gameObject, this.muzzleString, false);
+                float spread_value = ((aimRay.direction.x) - (aimRay.direction.x * 3f));
                 //EffectManager.SimpleMuzzleFlash(EntityStates.Mage.Weapon.FireLaserbolt.impactEffectPrefab, base.gameObject, this.muzzleString, false);
-                Vector3 raygun1 = Vector3.Cross(base.characterBody.transform.forward, base.characterBody.transform.right);
-                //Vector3 raygun2 = Vector3.Cross(Vector3.right, aimRay.origin).normalized;
+                Vector3 raygun1 = new Vector3(aimRay.direction.x + 0.2f,aimRay.direction.y,aimRay.direction.z);
+                Vector3 raygun2 = new Vector3(aimRay.direction.x - 0.2f, aimRay.direction.y, aimRay.direction.z);
                 //Vector3 raygun3 = Vector3.Cross(Vector3.left, aimRay.origin).normalized;
 
                 if (base.isAuthority)
                 {
-                    ProjectileManager.instance.FireProjectile(Materials.RegisterProjectiles.TestSkill, aimRay.origin, Util.QuaternionSafeLookRotation(raygun1), base.gameObject, this.damageCoefficient * this.damageStat, 0f, Util.CheckRoll(this.critStat, base.characterBody.master), DamageColorIndex.Default, null, -1f);
-                    //ProjectileManager.instance.FireProjectile(Materials.RegisterProjectiles.TestSkill, aimRay.origin, Util.QuaternionSafeLookRotation(raygun2), base.gameObject, this.damageCoefficient * this.damageStat, 0f, Util.CheckRoll(this.critStat, base.characterBody.master), DamageColorIndex.Default, null, -1f);
-                    //ProjectileManager.instance.FireProjectile(Materials.RegisterProjectiles.TestSkill, aimRay.origin, Util.QuaternionSafeLookRotation(raygun3), base.gameObject, this.damageCoefficient * this.damageStat, 0f, Util.CheckRoll(this.critStat, base.characterBody.master), DamageColorIndex.Default, null, -1f);
 
+                    ProjectileManager.instance.FireProjectile(Materials.RegisterProjectiles.TestSkill, aimRay.origin, Util.QuaternionSafeLookRotation(raygun1.normalized), base.gameObject, this.damageCoefficient * this.damageStat, 0f, Util.CheckRoll(this.critStat, base.characterBody.master), DamageColorIndex.Default, null, -1f);
+                    ProjectileManager.instance.FireProjectile(Materials.RegisterProjectiles.TestSkill, aimRay.origin, Util.QuaternionSafeLookRotation(raygun2.normalized), base.gameObject, this.damageCoefficient * this.damageStat, 0f, Util.CheckRoll(this.critStat, base.characterBody.master), DamageColorIndex.Default, null, -1f);
+                    ProjectileManager.instance.FireProjectile(Materials.RegisterProjectiles.TestSkill, aimRay.origin, Util.QuaternionSafeLookRotation(aimRay.direction), base.gameObject, this.damageCoefficient * this.damageStat, 0f, Util.CheckRoll(this.critStat, base.characterBody.master), DamageColorIndex.Default, null, -1f);
+                    Chat.AddMessage(aimRay.direction.ToString());
 
 
                     /*
